5ce09d3315cf0c7910695c3b934028b04e516f73
Ankit Narayan Choudhary <ankitnarayanchoudhary@192.168.1.21>
2026-03-26 21:21:38 +0530

    fixed mongodb

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 export async function connectDb(uri) {
+  let uri=await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
   if (!uri) throw new Error("Missing MONGODB_URI");
   mongoose.set("strictQuery", true);
   await mongoose.connect(uri);
